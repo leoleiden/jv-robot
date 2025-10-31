20e094d9ac1a9d31aa415059cc557dfccdd89df5
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-10-31 22:06:34 +0000

    Add movement logic for Robot to specified coordinates
    Implemented movement logic for the robot to reach specified coordinates.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,46 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        // Рух по осі X
+        if (robot.getX() < toX) {
+            // Поворот у напрямку RIGHT
+            while (robot.getDirection() != Direction.RIGHT) {
+                robot.turnRight();
+            }
+            // Рух вперед до досягнення координати toX
+            while (robot.getX() < toX) {
+                robot.stepForward();
+            }
+        } else if (robot.getX() > toX) {
+            // Поворот у напрямку LEFT
+            while (robot.getDirection() != Direction.LEFT) {
+                robot.turnLeft();
+            }
+            // Рух вперед до досягнення координати toX
+            while (robot.getX() > toX) {
+                robot.stepForward();
+            }
+        }
+
+        // Рух по осі Y
+        if (robot.getY() < toY) {
+            // Поворот у напрямку UP
+            while (robot.getDirection() != Direction.UP) {
+                robot.turnRight();
+            }
+            // Рух вперед до досягнення координати toY
+            while (robot.getY() < toY) {
+                robot.stepForward();
+            }
+        } else if (robot.getY() > toY) {
+            // Поворот у напрямку DOWN
+            while (robot.getDirection() != Direction.DOWN) {
+                robot.turnLeft();
+            }
+            // Рух вперед до досягнення координати toY
+            while (robot.getY() > toY) {
+                robot.stepForward();
+            }
+        }
     }
 }
